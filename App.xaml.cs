c133daaae16d0efc7b9ff5748b4b703a0a7a003d
Jacob Mordon <112067205+YakubMordon@users.noreply.github.com>
2024-02-09 15:03:17 +0200

    Added Haley for better window styling

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,6 +9,11 @@ namespace CryptoApp
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var startWindow = new StartWindow();
+            startWindow.ShowDialog();
+        }
     }
 
 }
