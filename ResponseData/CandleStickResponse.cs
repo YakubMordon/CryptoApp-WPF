430656677d54db0c20459c65edd8229554339c62
Jacob Mordon <112067205+YakubMordon@users.noreply.github.com>
2024-02-10 10:43:30 +0200

    Created ResponseData

---
+++ ResponseData/CandleStickResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoApp.ResponseData
+{
+    public class CandlestickResponse
+    {
+        public double Open { get; set; }
+        public double High { get; set; }
+        public double Low { get; set; }
+        public double Close { get; set; }
+        public double Volume { get; set; }
+        public long Period { get; set; }
+    }
+}
---
+++ ResponseData/MarketResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoApp.ResponseData
+{
+    public class MarketResponse
+    {
+        public string ExchangeId { get; set; }
+        public string BaseId { get; set; }
+        public string QuoteId { get; set; }
+        public string BaseSymbol { get; set; }
+        public string QuoteSymbol { get; set; }
+        public decimal VolumeUsd24Hr { get; set; }
+        public decimal PriceUsd { get; set; }
+        public decimal VolumePercent { get; set; }
+    }
+}
